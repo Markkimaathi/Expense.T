3055c5f4397a6135bc1f1d27fa2323ae40006eb3
Mark kimaaathi <markangelkimathi@gmail.com>
2024-09-03 20:55:07 +0300

    routes complete

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,3 +1,4 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/Income');
 
 const router = require('express').Router();
@@ -6,6 +7,9 @@ const router = require('express').Router();
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
     .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpense)
+    .delete('/delete-expense/:id', deleteExpense)
 
 
 module.exports = router
